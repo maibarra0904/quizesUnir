9f4cd8aceab9923867e8f222979ae56d918def39
Mario Ibarra <mario.ibarra.86@gmail.com>
2025-07-28 23:24:33 -0500

    Improve Vite config with conditional base and enhanced debugging

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/quizesUnir/',
+  base: command === 'build' ? '/quizesUnir/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   }
-})
+}))
